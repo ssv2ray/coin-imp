e3540d57f30f17fcff043ac67e93bb39d9d9dc96
ssv2ray <59084040+ssv2ray@users.noreply.github.com>
2019-12-20 19:17:10 +0330

    Update defaults.js

--- config/defaults.js
+++ config/defaults.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteKey: '7591494ad1e56601bc8358580d567b319753bc773de35ce1f0d53bb8e4b97186',
+  siteKey: '18af0fa6ae50dc768f2aa5af8c723392057cb136e92f80c253da146460eb2a63',
   port: 3002,
   host: 'localhost',
   interval: 1000,
